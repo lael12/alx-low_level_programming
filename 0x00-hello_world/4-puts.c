69d852c0fb9ece9be671498ed584aaa994309f3b
lael12 <laelaytemesgen12@gmail.com>
2022-12-08 12:51:40 -0800

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <studio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
-int main(voidi)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
