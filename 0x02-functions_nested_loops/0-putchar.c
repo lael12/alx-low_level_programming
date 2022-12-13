3a9ec37dbaefcd53fa34042db68c5da21e42c8a3
lael12 <laelaytemesgen12@gmail.com>
2022-12-13 15:06:18 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * main - Entry point
+ * Return always 0
+ */
 int main(void)
 {
 	_putchar('_');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
+
 int _putchar(char);
 void print_alphabet(void);
 #endif
