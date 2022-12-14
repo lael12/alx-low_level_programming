1de0913e421e9c6eaa2755000e66fe503da27724
lael12 <laelaytemesgen12@gmail.com>
2022-12-13 17:03:15 -0800

    correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,16 +11,10 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c >= 90))
 	{
 		return (1);
 	}
-	else if (c >= 65 && c >= 90)
-	{
-		return (1);
-	}
-	else
-	{
 	return (0);
-	}
+	
 }
