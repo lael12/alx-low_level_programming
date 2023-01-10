78d7473691d9f5b04e1246e9bf78e137e070411d
lael12 <laelaytemesgen12@gmail.com>
2023-01-10 11:54:33 -0800

    correction with advanced tasks

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,91 @@
+#include "main.h"
+
+/**
+ * word_len - Locates the delimiter index marking the end
+ * of the first word contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: length of word delimmited by space
+ */
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while ((str[index]) && (str[index] != ' '))
+	{
+		len++;
+		index++;
+	}
+	return (len);
+}
+
+/**
+ * count_word - count the number of words in a string
+ * @str: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_word(char *str)
+{
+	int w = 0;
+
+	while (*str != '\0')
+	{
+		/* skip spaces */
+		if (*str == ' ')
+			str++;
+		else
+		{
+			/* count words */
+			while (*str != ' ' && *str)
+			{
+				str++;
+			}
+			w++;
+		}
+	}
+	return (w);
+}
+
+/**
+* strtow - splits a string into words
+* Each element of array should contain a single word, null-terminated
+* @str: string parameter
+*
+* Return: pointer to array of strings(words)
+* NULL if str == NULL or str == "" or if your function fails
+*/
+
+char **strtow(char *str)
+{
+	char **ptr;
+	int index = 0, words, letters, a, l;
+
+	if (!str)
+		return (NULL);
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(char *) * (words + 1));
+	if (!ptr)
+		return (NULL);
+	for (a = 0; a < words; a++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+		ptr[a] = malloc(sizeof(char) * (letters + 1));
+		if (!ptr[a])
+		{
+			for (index -= 1; index >= 0; index--)
+				free(ptr[index]);
+			free(ptr);
+			return (NULL);
+		}
+		for (l = 0; l < letters; l++)
+		{
+			ptr[a][l] = str[index];
+
